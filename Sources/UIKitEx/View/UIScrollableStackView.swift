8d4f234745cbbd05d0964ab3da683cd1b6f45708
ObuchiYuki <yukibochi1@gmail.com>
2024-08-05 13:10:43 +0900

    sync

--- Sources/UIKitEx/View/UIScrollableStackView.swift
+++ Sources/UIKitEx/View/UIScrollableStackView.swift
@@ -39,25 +39,44 @@ open class UIScrollableStackView: UILoadScrollView {
         self.stackView.setCustomSpacing(spacing, after: view)
     }
         
+    open override var contentInset: UIEdgeInsets {
+        didSet {
+            self.leadingConstraint?.constant = self.contentInset.left
+            self.trailingConstraint?.constant = -self.contentInset.right
+            self.setNeedsUpdateConstraints()
+        }
+    }
+    
     public let stackView = UIStackView()
+    
+    private var trailingConstraint: NSLayoutConstraint?
+    
+    private var leadingConstraint: NSLayoutConstraint?
     
     open override func onAwake() {
         self.stackView.distribution = .equalSpacing
-        self.stackView.isUserInteractionEnabled = false
+        self.stackView.isUserInteractionEnabled = true
         self.stackView.axis = .vertical
         self.addSubview(self.stackView)
         self.contentInset = .zero
         
         self.stackView.translatesAutoresizingMaskIntoConstraints = false
         self.stackView.topAnchor.constraint(equalTo: self.contentLayoutGuide.topAnchor).isActive = true
-        self.stackView.leadingAnchor.constraint(equalTo: self.contentLayoutGuide.leadingAnchor).isActive = true
-        self.stackView.trailingAnchor.constraint(equalTo: self.contentLayoutGuide.trailingAnchor).isActive = true
+        
+        let leadingConstraint = self.stackView.leadingAnchor.constraint(equalTo: self.frameLayoutGuide.leadingAnchor)
+        leadingConstraint.isActive = true
+        self.leadingConstraint = leadingConstraint
+        
+        let trailingConstraint = self.stackView.trailingAnchor.constraint(equalTo: self.frameLayoutGuide.trailingAnchor)
+        trailingConstraint.isActive = true
+        self.trailingConstraint = trailingConstraint
+        
         do {
             let bottom = self.stackView.bottomAnchor.constraint(equalTo: self.contentLayoutGuide.bottomAnchor)
             bottom.priority = .defaultLow
             bottom.isActive = true
         }
         
-        self.stackView.widthAnchor.constraint(equalTo: self.frameLayoutGuide.widthAnchor).isActive = true
+//        self.stackView.widthAnchor.constraint(equalTo: self.frameLayoutGuide.widthAnchor).isActive = true
     }
 }
